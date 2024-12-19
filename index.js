e7675c02a27524744fd2c8c6c81836ddfc86fe93
widiapuspitasar <puspitasariwidia@gmail.com>
2024-12-19 15:25:46 +0700

    edit index js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server"); 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; 
 
-module.exports = (req, res) => {
-  server.use(middlewares);
-  server.use(router);
-  server(req, res);
-};
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
